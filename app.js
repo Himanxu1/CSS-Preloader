212d71c532dee55adeeac75d1f769ce1af9c26f9
Himanshu <technophilic000@gmail.com>
2022-03-09 21:10:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+//  function that get rids of preload
+
+window.addEventListener('load',() => {
+    const preloads = document.querySelector('.preload');
+    preloads.classList.add("preload-finish")
+});
